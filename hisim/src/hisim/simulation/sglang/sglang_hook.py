e49fc7d3d7d9dc065abf147fb4f73f004ff9a3a6
chenjie <chenjie137@huawei.com>
2026-07-17 17:30:55 +0800

    fix int

--- hisim/src/hisim/simulation/sglang/sglang_hook.py
+++ hisim/src/hisim/simulation/sglang/sglang_hook.py
@@ -567,11 +567,11 @@ class C_HiCacheController(BaseHook):
     @staticmethod
     def calc_prefetch_pages(
         required_pages: int, page_size_byte: int, max_dur: float, bandwidth: float
-    ) -> tuple[float, float]:
+    ) -> tuple[int, float]:
         _prefetch_dur = required_pages * page_size_byte / bandwidth
         if _prefetch_dur > max_dur:
-            _completed_pages = max(max_dur * bandwidth / page_size_byte, 1)
-            return _completed_pages, max_dur
+            _completed_pages = int(max_dur * bandwidth / page_size_byte)
+            return max(_completed_pages, 1), max_dur
         else:
             return required_pages, _prefetch_dur
 
@@ -648,7 +648,7 @@ class C_HiCacheController(BaseHook):
                     )
                 )
                 if completed_tokens < storage_hit_count - operation.completed_tokens:
-                    operation.completed_tokens += completed_tokens
+                    operation.completed_tokens += int(completed_tokens)
                     remain_dur = 0
                 else:
                     operation.completed_tokens = int(storage_hit_count)
